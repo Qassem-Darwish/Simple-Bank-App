eb1377eadb47aa76af2db0f08b53c92c79e6eb68
Qassem-Darwish <qasm83425@gmail.com>
2025-06-13 03:31:34 +0300

    BS-14 add comments to Example_of_Exception.java

--- src/example_of_exception/Example_Of_Exception.java
+++ src/example_of_exception/Example_Of_Exception.java
@@ -1,32 +1,33 @@
-
 package example_of_exception;
-import java.util.*;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Example_Of_Exception {
 
-    
+
     public static void main(String[] args) {
-       try{ 
-        Scanner in = new Scanner(System.in);
-        Non_Name_Bank ac1 = new Non_Name_Bank();
-        String option = "";
-        
-        do{
-            System.out.println("1. DEPOSIT");
-            System.out.println("2. WITHDRAW");
-            System.out.println("3. BALANCE ENQUIRY");
+        try {
+            Scanner in = new Scanner(System.in);
+            Non_Name_Bank ac1 = new Non_Name_Bank();
+            String option = "";
+
+            do {
+                System.out.println("1. DEPOSIT");
+                System.out.println("2. WITHDRAW");
+                System.out.println("3. BALANCE ENQUIRY");
                 System.out.print("ENTER OPTION : ");
                 option = in.next();
-                
-                switch(option){
-                    case"1":{
+
+                switch (option) {
+                    case "1": {
                         System.out.print("ENTER YOUR DEPOSIT AMOUNT : ");
                         double amt = in.nextDouble();
                         ac1.depoist(amt);
                         ac1.balanceEnquiry();
                         break;
                     }
-                    case"2":{
+                    case "2": {
                         System.out.print("ENTER WITHDRAW AMOUNT : ");
                         double amt = in.nextDouble();
                         double wd = ac1.Withdrwa(amt);
@@ -34,29 +35,32 @@ public class Example_Of_Exception {
                         ac1.balanceEnquiry();
                         break;
                     }
-                    case"3":{
+                    case "3": {
                         ac1.balanceEnquiry();
                         break;
                     }
-                    default:{
+                    default: {
                         System.out.println("BAD ENTRY");
                     }
                 }
-                
+
                 System.out.println("DO YOU WANT TO CONTINUE (YES/NO)");
                 option = in.next();
-                
-        }while(option.equalsIgnoreCase("YES"));
-       }catch(InValidAmountException iae){
-           System.out.println(iae.getMessage());
-       }catch(InsufficientFundsException ife){
-           System.out.println(ife.getMessage());
-       }catch(InputMismatchException ime){
-           System.out.println(ime.getMessage());
-       }catch(NumberFormatException nfe){
-           System.out.println(nfe.getMessage());
-       }
-        
+
+            } while (option.equalsIgnoreCase("YES"));
+        } catch (InValidAmountException iae) {
+            System.out.println(iae.getMessage());
+        } catch (InsufficientFundsException ife) {
+            // InsufficientFundsException is a custom exception
+            System.out.println(ife.getMessage());
+        } catch (InputMismatchException ime) {
+            // InputMismatchException is a custom exception
+            System.out.println(ime.getMessage());
+        } catch (NumberFormatException nfe) {
+            // NumberFormatException is not a custom exception
+            System.out.println(nfe.getMessage());
+        }
+
     }
-    
+
 }
